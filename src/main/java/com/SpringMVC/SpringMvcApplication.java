32931bdc0d67a83892f156f861b04d15d8e54993
Mehmet <m.sozdens@gmail.com>
2021-11-12 23:40:02 +0300

    day1

---
+++ src/main/java/com/SpringMVC/SpringMvcApplication.java
@@ -0,0 +1,19 @@
+package com.SpringMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan({"controller"})
+public class SpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcApplication.class, args);
+	}
+	//@ComponentScan ek açıklaması, Spring tarafından taranacak üç paketi (ve alt paketleri) belirtmek için basePackages özniteliğini kullanır
+	//@ComponentScan({"controller"}) //run class ı sonuc class ıyla farklı package de ve
+	//aynı hizada old için çalışabilmesi için bunu ekledik, aynı package de olsalar buna gerek yoktu.
+}
+//runner class
